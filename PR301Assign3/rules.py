1ed6460ef965d2b4ea7b665343f2fa5d9b193395
SamueIC <30849842+SamueIC@users.noreply.github.com>
2017-11-03 14:53:36 +1300

    Update rules.py
    This change is just a quick initial test. No point in creating all classes for each rule until I am sure that this structure works as it is. I have used the primitive type " None ". In PyCharm my expected output matched my actual so I was happy with these results.

--- PR301Assign3/rules.py
+++ PR301Assign3/rules.py
@@ -1,23 +1,67 @@
 # from validator import IFileValidator
+from abc import ABCMeta, abstractmethod
 
 
 class Rules(object):
     def __init__(self):
-        self.rules = {'id_rule': "^[A-Z][0-9]{3}$",
-                      'gender_rule': "^(M|F)$",
-                      'age_rule': "^[0-9]{2}$",
-                      'sales_rule': "^[0-9]{3}$",
-                      'bmi_rule': "^(Normal|Overweight|Obesity|Underweight)$",
-                      'salary_rule': "^[0-9]{2,3}$",
-                      'birthday_rule': "^[1-31]-[1-12]-[0-9]{4}$"
+        self.rules = {'id_rule': IDRule().rule,
+                      'gender_rule': None,
+                      'age_rule': None,
+                      'sales_rule': None,
+                      'bmi_rule': None,
+                      'salary_rule: None,
+                      'birthday_rule': None
                       }
 
-    def get_attr(self, arg):
-        return self.rules[arg]
 
-# test dictionary and it's method work correctly
+# Abstract Rule Class, #
+# Not sure what methods are abstract in this class yet#
+class Rule(metaclass=ABCMeta):
+    def __init__(self):
+        self.rule = None
 
-# a = Rules()
-# b = a.get_attr('id_rule')
-# print(a.rules)
-# print(b)
+    def get_rule(self):
+        return self.rule
+
+    def change_rule(self, new_rule):
+        self.rule = new_rule
+
+
+# Concrete Rule Classes #
+
+
+class IDRule(Rule):
+    def __init__(self):
+        super().__init__()
+        self.rule = "TEST IDRULE"
+
+# Tests to see my logic works # 
+
+a = Rules()
+print(a)
+b = a.rules
+print(b)
+
+ # Expected output: "TEST IDRULE"
+
+# ## OLD RULES CLASS BEFORE DESIGN PATTERN ASSIGNMENT 3 ############################
+# class Rules(object):
+#     def __init__(self):
+#         self.rules = {'id_rule': "^[A-Z][0-9]{3}$",
+#                       'gender_rule': "^(M|F)$",
+#                       'age_rule': "^[0-9]{2}$",
+#                       'sales_rule': "^[0-9]{3}$",
+#                       'bmi_rule': "^(Normal|Overweight|Obesity|Underweight)$",
+#                       'salary_rule': "^[0-9]{2,3}$",
+#                       'birthday_rule': "^[1-31]-[1-12]-[0-9]{4}$"
+#                       }
+#
+#     def get_attr(self, arg):
+#         return self.rules[arg]
+#
+#
+# #    def get_attr(self, arg):
+# #        return self.rules[arg]
+#
+# # test dictionary and it's method work correctly
+# ##################################################################################
